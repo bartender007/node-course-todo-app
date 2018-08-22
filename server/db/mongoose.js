44c69371f2d48c0c7d53a9cdb5cf631a229a7623
Michael Fleahman <mfleahman@gmail.com>
2018-08-22 17:42:35 -0400

    Add POST /todos route and refactored folders

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
+const options = {
+    useNewUrlParser: true
+}
+
 mongoose.Promise = global.Promise
-mongoose.connect('monodb://localhost:27017/TodoApp')
+mongoose.connect('mongodb://localhost:27017/TodoApp', options)
 
 module.exports = {mongoose} // Property and variable have the same name
